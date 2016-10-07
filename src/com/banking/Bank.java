f4889e72eacc03b52565b12190e642068b5047c4
ColorfulNight <q649931770@gmail.com>
2016-10-07 15:51:47 +0800

    Banking项目4：实现银行与顾客的一对多关系

---
+++ src/com/banking/Bank.java
@@ -0,0 +1,27 @@
+package com.banking;
+
+public class Bank {
+	private Customer[] customer;
+	private int numberOfCustomer = 0;
+
+	// 构造器初始化customer数组
+	public Bank() {
+		customer = new Customer[5];
+	}
+
+	// 添加顾客到customer数组
+	public void addCustomer(String f, String l) {
+		customer[numberOfCustomer] = new Customer(f, l);
+		numberOfCustomer+=1;
+	}
+
+	// 返回customer数组已有的顾客数量
+	public int getNumOfCustomers() {
+		return this.numberOfCustomer;
+	}
+
+	// 返回customer数组指定的顾客对象
+	public Customer getCustomer(int index) {
+		return customer[index];
+	}
+}
--- src/com/banking/Testbanking.java
+++ src/com/banking/Testbanking.java
@@ -3,24 +3,20 @@ package com.banking;
 public class Testbanking {
 
 	public static void main(String[] args) {
-		Customer customer;
-		Account account;
-
-		// Create an account that can has a 500.00 balance.
-		System.out.println("Creating the customer Jane Smith.");
-		customer = new Customer("Jane", "Smith");
-		System.out.println("Creating her account with a 500.00 balance.");
-		account = new Account(500.00);
-		customer.setAccount(account);
-		// Perform some account transactions
-		System.out.println("Withdraw 150.00: " + account.withdraw(150.00));
-		System.out.println("Deposit 22.50: " + account.deposit(22.50));
-		System.out.println("Withdraw 47.62: " + account.withdraw(47.62));
-		System.out.println("Withdraw 400.00: " + account.withdraw(400.00));
-
-		// Print out the final account balance
-		System.out.println("Customer [" + customer.getLastName() + ", " + customer.getFirstName()
-				+ "] has a balance of " + account.getBalance());
+		Bank bank = new Bank();
+		// Add Customer Jane, Simms
+		bank.addCustomer("Jane", "Simms");
+		// Add Customer Owen, Bryant
+		bank.addCustomer("Owen", "Bryant");
+		// Add Customer Tim, Soley
+		bank.addCustomer("Tim", "Soley");
+		// Add Customer Maria, Soley
+		bank.addCustomer("Maria", "Soley");
+		for (int i = 0; i < bank.getNumOfCustomers(); i++) {
+			Customer customer = bank.getCustomer(i);
+			System.out.println(
+					"Customer [" + (i + 1) + "] is " + customer.getLastName() + ", " + customer.getFirstName());
+		}
 	}
 
 }
